7df4c4a1143d8434b9bd53d7c01d7f9732799555
AndrewCloer <144556538+AndrewCloer@users.noreply.github.com>
2023-11-21 09:09:19 +0000

    added form section to my website that allows users to enter their name, email and a message. It also links their email to their name in their posted respones and allows me to remove posted responses

--- js/index.js
+++ js/index.js
@@ -4,7 +4,7 @@ const year = today.getFullYear();
 const footer = document.querySelector('footer');
 const copyright = document.createElement('p');
 //Adding name to footer
-copyright.innerHTML = 'Andrew Cloer' + year;
+copyright.innerHTML = 'Andrew Cloer ' + year;
 footer.appendChild(copyright);
 //Adding skills list and connecting it to html
 const skills = ['Basic JavaScript', 'Basic HTML'];
