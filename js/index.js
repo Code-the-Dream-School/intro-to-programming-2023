1668c4e29e4972414dbb8b76091c74b9e4c863f9
Dewi Anggraini <nn.dewi.anggraini@gmail.com>
2023-11-22 22:59:23 -0800

    adding changes for revisions lesson 4.2

--- js/index.js
+++ js/index.js
@@ -2,13 +2,15 @@ const today = new Date();
 const thisYear = today.getFullYear();
 const footer = document.querySelector('footer');
 const copyright = document.createElement('p');
-copyright.innerHTML = `Dewi Anggraini${thisYear}`;
+copyright.innerHTML = `Dewi Anggraini ${thisYear}`;
 footer.appendChild(copyright);
-const skills = ["JavaScript", "HTML", "CSS", "Problem solving"];
-const skillsSection = document.getElementById("skills");
-const skillsList = skillsSection.getElementById("UnList");
-for (let i = 0; i < skills.length; ++i){
+const skills = ['JavaScript', 'HTML', 'CSS', 'Problem solving'];
+const skillsSection = document.getElementById('skills');
+const skillsList = skillsSection.querySelector('ul');
+for (let i of skills){
+    console.log(i);
     let skill = document.createElement('li');
     skill.innerText = skills[i];
     skillsList.appendChild(skill);
 }
+    
