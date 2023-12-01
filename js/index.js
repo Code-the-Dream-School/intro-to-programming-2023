4da3dd1a8641521686bf4f9819042fc53692caca
Dewi Anggraini <nn.dewi.anggraini@gmail.com>
2023-11-30 23:43:04 -0800

    lesson 4.3

--- js/index.js
+++ js/index.js
@@ -13,4 +13,26 @@ for (const item of skills){
     skill.innerText = item;
     skillsList.appendChild(skill);
 }
-    
+let messageSection = document.getElementById('messages');
+let messageList = messageSection.querySelector('ul');
+let newMessage = document.createElement('li');
+newMessage.getElementByName('usersName').innerHTML = "<a href=mailto: + newMessage.getElementByName('usersEmail').email>";
+removeButton.innerText = 'remove';
+removeButton.Type = 'button';
+removeButton.addEventListener('click', () => {
+    let entry = document.getElementByType('button').parentNode.nodeName;
+    entry.remove();
+    newMessage.appendChild(removeButton);
+    messageList.appendChild(newMessage);
+});
+messageForm.addEventListener("submit", function(event){
+document.getElementsByName('leave_message').reset();    
+event.preventDefault();
+    let usersName = event.target.usersName;
+    let usersEmail = event.target.usersEmail;
+    let usersMessage = event.target.usersMessage;
+    console.log(usersName);
+    console.log(usersEmail);
+    console.log(usersMessage);
+ }, true);   
+const messageForm = document.getElementsByName('leave_message');
