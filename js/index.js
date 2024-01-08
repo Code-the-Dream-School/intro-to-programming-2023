4342257ac32c0b342f202737624a0fc5d250be08
Dewi Anggraini <nn.dewi.anggraini@gmail.com>
2024-01-07 20:28:47 -0800

    added changes to complete 4.3

--- js/index.js
+++ js/index.js
@@ -14,8 +14,9 @@ for (const item of skills){
     skillsList.appendChild(skill);
 }
 //Handle Message Form Submit
-const messageForm = document.getElementById('leave_message');
-messageForm.addEventListener("submit", function(event){    
+const messageForm = document.querySelector('form[name="leave_message"]');
+const input = messageForm.querySelector("input");
+messageForm.addEventListener("submit", (event) => {    
 
     event.preventDefault();
     
@@ -23,25 +24,42 @@ messageForm.addEventListener("submit", function(event){
     const usersEmail = event.target.usersEmail.value;
     const usersMessage = event.target.usersMessage.value;
     
+    const ul = document.getElementById('responds');
+    const li = document.createElement('li');
+    ul.appendChild(li);
+
     console.log('Name', usersName);
     console.log('Email', usersEmail);
     console.log('Message', usersMessage);
 
-    messageForm.reset();
- }, true);
+    const messageSection = document.getElementById('messages');
+    const messageList = messageSection.querySelector('ul');
+    const newMessage = document.createElement('li');
+    const usersLink = document.createElement('a');
+    usersLink.href = 'mailto:' + usersEmail;
+    usersLink.textContent = usersName;
+
+    const messageSpan = document.createElement('span');
+    messageSpan.textContent = usersMessage;
+
+    li.appendChild(usersLink);
+    li.appendChild(document.createTextNode(':'));
+    li.appendChild(messageSpan);
+    ul.appendChild(li);
+
+// remove button
+    const removeButton = document.createElement('button');
+    removeButton.textContent = 'remove';
+    li.appendChild(removeButton); 
+    messageList.appendChild(newMessage);
+    messageForm.reset();  
+ }, required);
 
  //Display Messages in List
-const messageSection = document.getElementById('messages');
-const messageList = messageSection.querySelector('ul');
-const newMessage = document.createElement('li');
-const removeButton = document.createElement('button');
-
-removeButton.addEventListener('click', () => {    
-    removeButton.innerText = 'remove';
-    removeButton.Type = 'button';
-    newMessage.innerHTML = `<a href="mailto:${usersEmail}">${usersName}</a><span>${usersMessage}</span>`;    
-    const entry = removeButton.parentNode;
-    entry.remove();
-    newMessage.appendChild(removeButton);
-    messageList.appendChild(newMessage);
+document.getElementById('responds').addEventListener('click', (event) => {    
+   if (event.target.tagName === 'BUTTON') {
+     const li = e.target.parentNode;
+     const ul = li.parentNode;
+     ul.removeChild(li);
+   }
 });
